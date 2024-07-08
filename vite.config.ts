a10a77e9f86f6065592112bc13ac76a3552f6b14
besthanzo <1580408405@qq.com>
2024-07-08 15:32:02 +0800

    feat: 尝试修改vite

--- vite.config.ts
+++ vite.config.ts
@@ -42,7 +42,7 @@ export default ({ command, mode }: ConfigEnv): UserConfig => {
   const isBuild = command === 'build'
   // command === 'build'
   return {
-    base: VITE_PUBLIC_PATH,
+    base: '/poetry-society/',
     root,
 
     // 别名
