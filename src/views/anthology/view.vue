59c7f1c4cf04607dc3d7b5bf09816ce9347a412f
besthanzo <1580408405@qq.com>
2024-07-09 15:07:31 +0800

    feat: fix Luj

--- src/views/anthology/view.vue
+++ src/views/anthology/view.vue
@@ -1,21 +1,20 @@
 <template>
     <div class="mb-6 mt-4 w-full">
-        <div v-for="poem in poems" :key="poem.id"
+        <!-- <div v-for="poem in poems" :key="poem.id"
             class="text-center mx-11 my-5 py-5 rounded-lg border border-slate-200 shadow-sm" >
             <h2 class="text-xl ">{{ poem.title }}</h2>
             <p > {{ poem.author }}</p>
-            <!-- <p class="text-wrap mx-6 text-sm"><strong>Tags:</strong> {{ poem.tags.join(', ') }}</p> -->
+           <p class="text-wrap mx-6 text-sm"><strong>Tags:</strong> {{ poem.tags.join(', ') }}</p> 
             <ul>
                 <li v-for="paragraph in poem.paragraphs" :key="paragraph">{{ paragraph }}</li>
             </ul>
-        </div>
+        </div> -->
     </div>
 </template>
 
 <script setup lang="ts">
-import poemsData from '@/assets/poem/曹操诗集/唐诗三百首.json'; // 导入 JSON 数据
-
-const poems = poemsData; // 将导入的数据赋值给变量
+// import poemsData from '@/assets/poem/曹操诗集/唐诗三百首.json'; // 导入 JSON 数据
+// const poems = poemsData; // 将导入的数据赋值给变量
 
 // 可以在这里添加其他逻辑或计算属性
 </script>
